296f01d73a0b1faefb60ea1ccec4c777025779dc
Dewaldt de Waal <34021263+Dewaldt18@users.noreply.github.com>
2023-09-12 11:57:19 +0200

    Publishing Pterodactyl I

---
+++ src/app/admin/auth.guard.ts
@@ -0,0 +1,11 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { inject } from '@angular/core';
+
+
+//Checks if our user is logged in
+export const authGuard: CanActivateFn = (route, state) => {
+  if(inject(AuthService).session) return true;
+  inject(Router).navigateByUrl('/')
+  return false;
+};
